3f75d1f84812234451c258ed4d534efeee902788
Me-ictosn <90956553+Me-ictosn@users.noreply.github.com>
2022-12-28 13:14:20 +0900

    feat: 右下のボタンを追加

--- tani-keisan/MainWindow.xaml.cs
+++ tani-keisan/MainWindow.xaml.cs
@@ -24,5 +24,49 @@ namespace tani_keisan
         {
             InitializeComponent();
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Change_Pos(object sender, RoutedEventArgs e)
+        {
+            var item = (MenuItem)sender;
+            double ScreenWidth = SystemParameters.WorkArea.Width;
+            double ScreenHeight = SystemParameters.WorkArea.Height;
+            switch (item.Header.ToString())
+            {
+                case "右上":
+                    this.Top = 0;
+                    this.Left = ScreenWidth - this.Width;
+                    break;
+                case "左上":
+                    this.Top = 0;
+                    this.Left = 0;
+                    break;
+                case "右下":
+                    this.Top = ScreenHeight;
+                    this.Left = ScreenWidth - this.Width;
+                    break;
+                case "左下":
+                    this.Top = ScreenHeight;
+                    this.Left = 0;
+                    break;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
