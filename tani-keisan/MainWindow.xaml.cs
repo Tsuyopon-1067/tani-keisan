25f656974cf42ffb7b1e71e1b73ae1132008b02d
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-12-30 23:46:15 +0900

    Add comment.

--- tani-keisan/MainWindow.xaml.cs
+++ tani-keisan/MainWindow.xaml.cs
@@ -61,6 +61,12 @@ namespace tani_keisan
         }
 
 
+        /// <summary>
+        /// ウィンドウを画面四隅（左上・右上・左下・右下）に移動するメソッド
+        /// </summary>
+        /// <param name="sender">object型だがMenuIteのみを受け取る前提</param>
+        /// <param name="e">おまじない イベントハンドラとして必要</param>
+
         private void Change_Pos(object sender, RoutedEventArgs e)
         {
             var item = (MenuItem)sender;
@@ -86,6 +92,11 @@ namespace tani_keisan
                     break;
             }
         }
+        /// <summary>
+        /// アプリケーションを終了するメソッド
+        /// </summary>
+        /// <param name="sender">おまじない イベントハンドラとして必要</param>
+        /// <param name="e">おまじない イベントハンドラとして必要</param>
         private void QuitButton_Click(object sender, RoutedEventArgs e)
         {
             timer.Stop();
@@ -94,17 +105,34 @@ namespace tani_keisan
 
         SolidColorBrush bottomBarColor = new SolidColorBrush(Color.FromArgb(0x00, 0xFF, 0x00, 0x00)); // 通常時の色(透明にしておく)
         SolidColorBrush bottomBarSelectedColor = new SolidColorBrush(Color.FromArgb(0xFF, 0xD9, 0xB5, 0xD4)); // 選択時の色 FF D9 B5 D4
-        
-        // 下部のボタンにマウスが重なると色が変わる
+
+        /// <summary>
+        /// 下部のボタンにマウスが重なった時に色を変えるメソッド
+        /// </summary>
+        /// <remarks> 背景色を選択時の色に変更する </remarks>
+        /// <param name="sender">おまじない イベントハンドラとして必要</param>
+        /// <param name="e">おまじない イベントハンドラとして必要</param>
         private void ButtomButton_MouseEnter(object sender, MouseEventArgs e)
         {
             ((TextBlock)sender).Background = bottomBarSelectedColor;
         }
-        // 下部のボタンからマウスが離れると色が戻る
+        /// <summary>
+        /// 下部のボタンからマウスが離れた時に色を戻すメソッド
+        /// </summary>
+        /// <remarks> 背景色を透明にする </remarks>
+        /// <param name="sender">おまじない イベントハンドラとして必要</param>
+        /// <param name="e">おまじない イベントハンドラとして必要</param>
         private void ButtomButton_MouseLeave(object sender, MouseEventArgs e)
         {
             ((TextBlock)sender).Background = bottomBarColor;
         }
+
+        /// <summary>
+        /// 位置ボタンが左クリックされた時にコンテキストメニューを表示するメソッド
+        /// </summary>
+        /// <remarks> 受け取ったButtonのContextMenuのIsOpenプロパティをtrueにする </remarks>
+        /// <param name="sender">object型だがButtonのみを受け取る前提</param>
+        /// <param name="e">おまじない イベントハンドラとして必要</param>
 
         private void OpenContextMenu(object sender, RoutedEventArgs e)
         {
@@ -118,11 +146,23 @@ namespace tani_keisan
         [DllImport("user32.dll")]
         public static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 
-
+        // https://learn.microsoft.com/ja-jp/windows/win32/inputdev/wm-nchittest
         private void ChangeWindowSize(int n)
         {
             IntPtr hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
             PostMessage(hwnd, 0xA1, (IntPtr)n, IntPtr.Zero);
+        }
+        private void WindowSizeLeft(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ChangeWindowSize(10);
+        }
+        private void WindowSizeRight(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ChangeWindowSize(11);
+        }
+        private void WindowSizeTop(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ChangeWindowSize(12);
         }
         private void WindowSizeLeftTop(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
@@ -132,6 +172,10 @@ namespace tani_keisan
         {
             ChangeWindowSize(14);
         }
+        private void WindowSizeBottom(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ChangeWindowSize(15);
+        }
         private void WindowSizeLeftBottom(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             ChangeWindowSize(16);
@@ -139,22 +183,6 @@ namespace tani_keisan
         private void WindowSizeRightBottom(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             ChangeWindowSize(17);
-        }
-        private void WindowSizeLeft(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            ChangeWindowSize(10);
-        }
-        private void WindowSizeTop(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            ChangeWindowSize(12);
-        }
-        private void WindowSizeRight(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            ChangeWindowSize(11);
-        }
-        private void WindowSizeBottom(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            ChangeWindowSize(15);
         }
     }
 }
