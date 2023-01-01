b17e517e9a2bd37b6f9acb6c7554649c0021094a
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-01-01 21:14:43 +0900

    feat: Create CreditResister window.
    単位登録ウィンドウを作成．

---
+++ tani-keisan/CreditInput.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace tani_keisan
+{
+    /// <summary>
+    /// CreditInput.xaml の相互作用ロジック
+    /// </summary>
+    public partial class CreditInput : UserControl
+    {
+        /// <summary>
+        /// TextBlock（左側）を変更するメソッド
+        /// </summary>
+        public string Text
+        {
+            get { return tb.Text; }
+            set { tb.Text = value; }
+        }
+
+        /// <summary>
+        /// TextBox（右側）を変更するメソッド
+        /// </summary>
+        public int Number { get; set; } = 0;
+        public CreditInput()
+        {
+            InitializeComponent();
+            tb.DataContext = Text;
+            input.DataContext = Number.ToString();
+        }
+
+        private void TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int tmp = 0;
+            int.TryParse(input.Text, out tmp);
+            Number = tmp;
+            MessageBox.Show(Text);
+        }
+    }
+}
