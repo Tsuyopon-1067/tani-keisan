8cc2c02aa50970dd50af364e01e0c13061f9b811
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-01-02 22:37:04 +0900

    add: Create SettingsSave class.

---
+++ tani-keisan/Properties/SettingsSave.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tani_keisan.Properties
+{
+    static class SettingsSave
+    {
+        //保存先のファイル名
+        private static string fileName = @"./settings.xml";
+        static DisplayedCredit dc;
+
+        public static void Save()
+        {
+
+            if (dc == null)
+            {
+                dc = new DisplayedCredit();
+            }
+
+            //XmlSerializerオブジェクトを作成
+            //オブジェクトの型を指定する
+            System.Xml.Serialization.XmlSerializer serializer =
+                new System.Xml.Serialization.XmlSerializer(typeof(DisplayedCredit));
+            //書き込むファイルを開く（UTF-8 BOM無し）
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(
+                fileName, false, new System.Text.UTF8Encoding(false));
+            //シリアル化し、XMLファイルに保存する
+            serializer.Serialize(sw, dc);
+            //ファイルを閉じる
+            sw.Close();
+        }
+        public static DisplayedCredit ReadDisplayedCredit() 
+        {
+            //XmlSerializerオブジェクトを作成
+            System.Xml.Serialization.XmlSerializer serializer =
+                new System.Xml.Serialization.XmlSerializer(typeof(DisplayedCredit));
+            //読み込むファイルを開く
+            System.IO.StreamReader sr = new System.IO.StreamReader(
+                fileName, new System.Text.UTF8Encoding(false));
+            //XMLファイルから読み込み、逆シリアル化する
+            DisplayedCredit obj = (DisplayedCredit)serializer.Deserialize(sr);
+            //ファイルを閉じる
+            sr.Close();
+
+            if (obj == null)
+            {
+                return new DisplayedCredit();
+            }
+            return obj;
+        }
+    }
+}
