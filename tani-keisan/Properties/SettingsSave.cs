1d611861f7bb98719174a00f26acf308dd80907e
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-01-02 22:49:28 +0900

    update: Change save path.

--- tani-keisan/Properties/SettingsSave.cs
+++ tani-keisan/Properties/SettingsSave.cs
@@ -9,10 +9,10 @@ namespace tani_keisan.Properties
     static class SettingsSave
     {
         //保存先のファイル名
-        private static string fileName = @"./settings.xml";
+        private static string dcFileName = @"./settings/DisplayedCredit.xml";
         static DisplayedCredit dc;
 
-        public static void Save()
+        public static void SaveDisplayedCredit()
         {
 
             if (dc == null)
@@ -26,7 +26,7 @@ namespace tani_keisan.Properties
                 new System.Xml.Serialization.XmlSerializer(typeof(DisplayedCredit));
             //書き込むファイルを開く（UTF-8 BOM無し）
             System.IO.StreamWriter sw = new System.IO.StreamWriter(
-                fileName, false, new System.Text.UTF8Encoding(false));
+                dcFileName, false, new System.Text.UTF8Encoding(false));
             //シリアル化し、XMLファイルに保存する
             serializer.Serialize(sw, dc);
             //ファイルを閉じる
@@ -39,7 +39,7 @@ namespace tani_keisan.Properties
                 new System.Xml.Serialization.XmlSerializer(typeof(DisplayedCredit));
             //読み込むファイルを開く
             System.IO.StreamReader sr = new System.IO.StreamReader(
-                fileName, new System.Text.UTF8Encoding(false));
+                dcFileName, new System.Text.UTF8Encoding(false));
             //XMLファイルから読み込み、逆シリアル化する
             DisplayedCredit obj = (DisplayedCredit)serializer.Deserialize(sr);
             //ファイルを閉じる
